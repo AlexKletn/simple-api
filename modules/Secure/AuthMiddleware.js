7077c950fe9571dcb4cb045d2af8df62a8a8e124
AlexQ <kletnalex98@gmail.com>
2021-08-31 12:23:51 +0300

    token fix

--- modules/Secure/AuthMiddleware.js
+++ modules/Secure/AuthMiddleware.js
@@ -6,15 +6,23 @@ export default async function AuthMiddleware(ctx, next) {
   const { token } = ctx.request;
 
   if (token) {
-    const payload = jwt.verify(token, jwtRSA.public);
+    let payload;
 
-    ctx.state.auth = User.findOne({
-      _id: payload.id,
-    }, {
-      passwordHash: 0,
-      tokens: 0,
-      salt: 0,
-    });
+    try {
+      payload = jwt.verify(token, jwtRSA.public);
+    } catch (e) {
+      payload = undefined;
+    }
+
+    if (payload) {
+      ctx.state.auth = User.findOne({
+        _id: payload.id,
+      }, {
+        passwordHash: 0,
+        tokens: 0,
+        salt: 0,
+      });
+    }
   } else {
     ctx.state.auth = null;
   }
