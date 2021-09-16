cc594eed0587f5af11436be6a40ab670da79d32e
AlexQ <kletnalex98@gmail.com>
2021-09-16 16:27:43 +0300

    docker&deploy-script change

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 ROOT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "${ROOT_FOLDER}/../.." || exit
+cd "${ROOT_FOLDER}/.." || exit
 git reset --hard
 git pull
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml down
+docker-compose -f docker-compose.yml up -d
