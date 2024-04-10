a4fe544fc1bf1fe689b63414bfb9e2eb8c21a8b5
Alex Kletnyy <q@obscur.space>
2024-04-10 17:01:14 +0300

    Revert "fix: db secure"
    This reverts commit dd8294ecf6c20e8377742d37d101f87d1a2f1383.

--- configs/storage.js
+++ configs/storage.js
@@ -1,13 +1,13 @@
 export const mongo = {
-  url: `${process.env.SIMPLE_DB_USERNAME}:${process.env.SIMPLE_DB_PASSWORD}@${
+  url: `${
     process.env.SIMPLE_DB_HOST
   }:${
     process.env.SIMPLE_DB_PORT
   }`,
   dbName: process.env.SIMPLE_DB_NAME,
 
-  // user: process.env.SIMPLE_DB_USERNAME,
-  // pass: process.env.SIMPLE_DB_PASSWORD,
+  user: process.env.SIMPLE_DB_USERNAME,
+  pass: process.env.SIMPLE_DB_PASSWORD,
 
   useUnifiedTopology: true,
   useNewUrlParser: true,
